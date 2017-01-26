9f82e8938c47f4f76189e6beaa18ce2d9b343f14
ShubhamBansal1997 <shubhambansal17@hotmail.com>
2017-01-27 00:25:47 +0530

    first commit

--- application/controllers/emailIntegration_resources/Infusionsoft/EmailAddStatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Infusionsoft_EmailAddStatus extends Infusionsoft_Generated_EmailAddStatus{
-    public function __construct($id = null, $app = null){
-    	parent::__construct($id, $app);    	    	
-    }
-}
-
